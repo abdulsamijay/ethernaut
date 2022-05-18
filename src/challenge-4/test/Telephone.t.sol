275dd10dc56e85adf0d3ed4ff921817715a594de
Abdul Sami J <isamjay007@gmail.com>
2022-05-18 17:38:33 +0500

    :tada: Added challenge #4

---
+++ src/challenge-4/test/Telephone.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "forge-std/Test.sol";
+import "../Telephone.sol";
+import "../TelephoneExploit.sol";
+
+contract ContractTest is Test {
+    Telephone t;
+    TelephoneExploit te;
+
+    function setUp() public {
+        t = new Telephone();
+        te = new TelephoneExploit(address(t));
+    }
+
+    function test_challenge_4() public {
+        console.log("Onwner Of telephone before", t.owner());
+
+        vm.startPrank(address(1337));
+        te.forwardCall(address(1337));
+        vm.stopPrank();
+
+        console.log("Onwner of telephone before", t.owner());
+    }
+}
